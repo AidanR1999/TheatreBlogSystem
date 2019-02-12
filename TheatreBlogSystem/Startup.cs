c9ad55e7bd46a2b4aa471036b9c74e297627f35d
Aidan Rooney <Aidanr1999@hotmail.com>
2019-02-12 10:59:07 +0000

    Add project files.

---
+++ TheatreBlogSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheatreBlogSystem.Startup))]
+namespace TheatreBlogSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
